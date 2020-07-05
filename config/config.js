35104b408a31e7e488d420f12cf1ba647af71bda
Micah Elijah <melijah200@gmail.com>
2020-07-05 11:33:35 +0100

    initial commit

--- config/config.js
+++ config/config.js
@@ -3,32 +3,19 @@ const mongoose = require("mongoose");
 const url = process.env.MONGO_URL;
 
 module.exports = connection = () => {
-  // mongoose.connect(
-  //   url,
-  //   {
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true,
-  //   },
-  //   (err, success) => {
-  //     if (err) {
-  //       console.log("Database connection failed!");
-  //       // process.exit();
-  //     } else {
-  //       console.log("Database connection success");
-  //     }
-  //   }
-  // );
-  mongoose
-    .connect(url, {
+  mongoose.connect(
+    url,
+    {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
-    })
-    .then(() => {
-      console.log("Successfully connected to the database");
-    })
-    .catch(err => {
-      console.log("Could not connect to the database. Exiting now...", err);
-      process.exit();
-    });
+    },
+    (err, success) => {
+      if (err) {
+        console.log("Database connection failed!");
+        // process.exit();
+      } else {
+        console.log("Database connection success");
+      }
+    }
+  );
 };
